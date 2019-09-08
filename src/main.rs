26a11c090560b127d3860a69a7d99d51305d51a7
Márcio <marciosr10@gmail.com>
2019-09-08 14:25:37 -0300

    Padroniza e organiza melhor o código

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,11 @@ pub struct MainWindow {
 	pub ent_express2: Entry,
 	pub bt_fechar: Button,
 	pub bt_run: Button,
-	pub bt_fs1: FileChooserButton,
-	pub bt_fs2: FileChooserButton,
-	pub bt_close: Button,
-	pub revealer: Revealer,
-	pub label2: Label,
+	pub bt_entrada: FileChooserButton,
+	pub bt_saida: FileChooserButton,
+	pub bt_fecha_notifica: Button,
+	pub rv_notifica: Revealer,
+	pub lb_notifica: Label,
 }
 
 impl MainWindow {
@@ -34,11 +34,11 @@ impl MainWindow {
 		let ent_saida: Entry = glade.get_object("ent_saida").unwrap();
 		let bt_fechar: Button = glade.get_object("bt_fechar").unwrap();
 		let bt_run: Button = glade.get_object("bt_run").unwrap();
-		let bt_fs1: FileChooserButton = glade.get_object("bt_fs1").unwrap();
-		let bt_fs2: FileChooserButton = glade.get_object("bt_fs2").unwrap();
-		let bt_close: Button = glade.get_object("button2").unwrap();
-		let revealer: Revealer = glade.get_object("revealer2").unwrap();
-		let label2: Label = glade.get_object("label2").unwrap();
+		let bt_entrada: FileChooserButton = glade.get_object("bt_entrada").unwrap();
+		let bt_saida: FileChooserButton = glade.get_object("bt_saida").unwrap();
+		let bt_fecha_notifica: Button = glade.get_object("bt_fecha_notifica").unwrap();
+		let rv_notifica: Revealer = glade.get_object("rv_notifica").unwrap();
+		let lb_notifica: Label = glade.get_object("label2").unwrap();
 
 		window.connect_delete_event(move |_,_| {
 			main_quit();
@@ -50,19 +50,19 @@ impl MainWindow {
 			Inhibit(false);
 		});
 
-		let btfs1 = bt_fs1.clone();
-		let btfs2 = bt_fs2.clone();
+		let bt_entrada_clone = bt_entrada.clone();
+		let bt_saida_clone = bt_saida.clone();
 		let ent_express1_clone = ent_express1.clone();
 		let ent_express2_clone = ent_express2.clone();
 		let ent_saida_clone = ent_saida.clone();
-		let revealer_clone = revealer.clone();
-		let label2_clone = label2.clone();
+		let rv_notifica_clone = rv_notifica.clone();
+		let lb_notifica_clone = lb_notifica.clone();
 
 		bt_run.connect_clicked(move |_| {
 
-			if Dados::check(&btfs1, &btfs2, &ent_express1_clone, &ent_express2_clone, &ent_saida_clone, &revealer_clone, &label2_clone) {
+			if Dados::check(&bt_entrada_clone, &bt_saida_clone, &ent_express1_clone, &ent_express2_clone, &ent_saida_clone, &rv_notifica_clone, &lb_notifica_clone) {
 
-			 	let dados = Dados::new(&btfs1, &btfs2, &ent_express1_clone, &ent_express2_clone, &ent_saida_clone);
+			 	let dados = Dados::new(&bt_entrada_clone, &bt_entrada_clone, &ent_express1_clone, &ent_express2_clone, &ent_saida_clone);
 			 	let texto = fs::read_to_string(&dados.uri_entrada);
 
 			 	match texto {
@@ -74,20 +74,19 @@ impl MainWindow {
 			 		},
 			 		Err(e)		=> {
 			 			println!("Erro no processamento do texto: {}", e);
-			 			label2_clone.set_label("A codificação do arquivo de entrada deve ser UTF-8! Converta-o em um editor de texto.");
-			 			revealer_clone.set_reveal_child(true);
+			 			lb_notifica_clone.set_label("A codificação do arquivo de entrada deve ser UTF-8! Converta-o em um editor de texto.");
+			 			rv_notifica_clone.set_reveal_child(true);
 			 		},
 			 	}
 			} else { println!("Faltam parâmetros!"); }
 		});
 
-		let revealer_clone2 = revealer.clone();
-
-		bt_close.connect_clicked(move |_| {
-			revealer_clone2.set_reveal_child(false);
+		let rv_notifica_clone2 = rv_notifica.clone();
+		bt_fecha_notifica.connect_clicked(move |_| {
+			rv_notifica_clone2.set_reveal_child(false);
 		});
 
-		MainWindow { glade, window, ent_express1, ent_express2, bt_fechar, bt_run, bt_fs1, bt_fs2, bt_close, revealer, label2 }
+		MainWindow { glade, window, ent_express1, ent_express2, bt_fechar, bt_run, bt_entrada, bt_saida, bt_fecha_notifica, rv_notifica, lb_notifica }
 	}
 }
 
@@ -104,8 +103,8 @@ fn main() {
     gtk::main();
 }
 
-fn analisa_memorial (uri1: PathBuf, uri2: PathBuf, expressao1: String,  expressao2: String) -> Result<()> {
-	let texto = fs::read_to_string(uri1)?; // Tirei o método .unwrap() e coloquei o operador ?
+fn analisa_memorial (uri_entrada: PathBuf, uri_saida: PathBuf, expressao_n: String,  expressao_e: String) -> Result<()> {
+	let texto = fs::read_to_string(uri_entrada)?; // Tirei o método .unwrap() e coloquei o operador ?
 	let text = &String::from(texto);
 
 	const VEC_SIZE: usize = 13;
@@ -114,18 +113,18 @@ fn analisa_memorial (uri1: PathBuf, uri2: PathBuf, expressao1: String,  expressa
 	// r"\d.\d{3}.\d{3},\d{3}" -> quando utilizado através do gtk_entry não foi
 	// necessário o caracteres r e as aspas, apenas
 	// a expressão regular propriamente dita.
-    for mat2 in Regex::new(&expressao1).unwrap().find_iter(text) {
-    	let start = mat2.start() as usize;
-    	let end = mat2.end() as usize;
+    for correspondencia in Regex::new(&expressao_n).unwrap().find_iter(text) {
+    	let start = correspondencia.start() as usize;
+    	let end = correspondencia.end() as usize;
     	vetor1.push(&text[start..end]);
     }
 
-	for mat2 in Regex::new(&expressao2).unwrap().find_iter(text) {
-    	let start = mat2.start() as usize;
-    	let end = mat2.end() as usize;
+	for correspondencia in Regex::new(&expressao_e).unwrap().find_iter(text) {
+    	let start = correspondencia.start() as usize;
+    	let end = correspondencia.end() as usize;
     	vetor2.push(&text[start..end]);
     }
-    gera_csv (vetor1, vetor2, uri2).expect("Não foi possível utilizar a uri informada pela função Dados::new()");
+    gera_csv (vetor1, vetor2, uri_saida).expect("Não foi possível utilizar a uri informada pela função Dados::new()");
     Ok(())
 }
 
@@ -154,69 +153,67 @@ struct Dados {
 
 #[allow(dead_code)]
 impl Dados {
-	fn new (btfs1:		&FileChooserButton,
-			btfs2:		&FileChooserButton,
+	fn new (bt_entrada:		&FileChooserButton,
+			bt_saida:		&FileChooserButton,
 			ent_exp1:	&Entry,
 			ent_exp2:	&Entry,
 			ent_nome:	&Entry)-> Dados {
 
-		let uri_entrada = btfs1.get_filename().unwrap();
+		let uri_entrada = bt_entrada.get_filename().unwrap();
 		println!("URI da entrada {:?}\n", uri_entrada);
 
-		let mut uri_saida = btfs2.get_filename().unwrap();
-
-		println!("URI saída é: {:?}", btfs2.get_filename().unwrap());
+		let mut uri_saida = bt_saida.get_filename().unwrap();
+		println!("URI saída é: {:?}", bt_saida.get_filename().unwrap());
 
 		let expressao_1: String = ent_exp1.get_text().unwrap().to_string();
 		let expressao_2: String = ent_exp2.get_text().unwrap().to_string();
 		let nome_csv: String = ent_nome.get_text().unwrap().to_string();
-		// uri_saida.push_str(&"/".to_string());
+
 		uri_saida.push(&nome_csv);
 		uri_saida.set_extension("csv");
-		//uri_saida.push_str(&nome_csv);
-	Dados { uri_entrada, uri_saida, expressao_1, expressao_2, nome_csv }
+
+		Dados { uri_entrada, uri_saida, expressao_1, expressao_2, nome_csv }
 	}
 
-	fn check (btfs1:		&FileChooserButton,
-			btfs2:		&FileChooserButton,
-			ent_exp1:	&Entry,
-			ent_exp2:	&Entry,
-			ent_nome:	&Entry,
-			revealer:	&Revealer,
-			label: 		&Label) -> bool {
+	fn check(	bt_entrada:		&FileChooserButton,
+				bt_saida:		&FileChooserButton,
+				ent_exp1:		&Entry,
+				ent_exp2:		&Entry,
+				ent_nome:		&Entry,
+				rv_notifica:	&Revealer,
+				lb_notifica: 	&Label) -> bool {
 
 			let mut resultado: bool = false;
 
 			if ent_nome.get_text_length() != 0 {
 				if ent_exp1.get_text_length() != 0 {
 					if ent_exp2.get_text_length() != 0 {
-						if let Some(teste) = btfs1.get_uri() {
-						 	println!("O widget bt_fs1 tem: {:?}", teste.as_str());
-						 	if let Some(teste) = btfs2.get_uri() {
-						 		println!("O widget bt_fs1 tem: {:?}", teste.as_str());
+						if let Some(teste) = bt_entrada.get_uri() {
+						 	println!("O widget bt_entrada tem: {:?}", teste.as_str());
+						 	if let Some(teste) = bt_saida.get_uri() {
+						 		println!("O widget bt_entrada tem: {:?}", teste.as_str());
 						 		resultado = true;
 						 	} else {
-								label.set_label("Selecione o diretório de destino!");
-								revealer.set_reveal_child(true);
+								lb_notifica.set_label("Selecione o diretório de destino!");
+								rv_notifica.set_reveal_child(true);
 					 		}
-
 						} else {
-							label.set_label("Selecione o arquivo a ser analisado!");
-							revealer.set_reveal_child(true);
+							lb_notifica.set_label("Selecione o arquivo a ser analisado!");
+							rv_notifica.set_reveal_child(true);
 						};
 
 					} else {
-						label.set_label("Informe a segunda expressão regular!");
-						revealer.set_reveal_child(true);
+						lb_notifica.set_label("Informe a segunda expressão regular!");
+						rv_notifica.set_reveal_child(true);
 					}
 
 				} else {
-					label.set_label("Informe a primeira expressão regular!");
-					revealer.set_reveal_child(true);
+					lb_notifica.set_label("Informe a primeira expressão regular!");
+					rv_notifica.set_reveal_child(true);
 				}
 			} else {
-				label.set_label("Informe o nome da planilha resultante!");
-				revealer.set_reveal_child(true);
+				lb_notifica.set_label("Informe o nome da planilha resultante!");
+				rv_notifica.set_reveal_child(true);
 			}
 
 		resultado
