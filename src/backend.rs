21016db7a8e14fc4159ba9f6455b0bcd2655833c
Márcio <marcio@archdesk.localdomain>
2020-09-05 19:13:44 -0300

    Cleanup the code

--- src/backend.rs
+++ src/backend.rs
@@ -1,5 +1,3 @@
-extern crate gtk;
-
 use std::fs::File;
 use std::fs;
 use std::io::prelude::*;
@@ -13,10 +11,10 @@ use regex::Regex;
 use csv::*;
 use serde::{Serialize, Deserialize};
 
-pub fn analisa_texto (  uri_entrada: PathBuf,
-                        uri_saida: PathBuf,
-                        expressao_n: String,
-                        expressao_e: String ) -> Result<()> {
+pub fn analisa_texto (  uri_entrada: 	PathBuf,
+                        uri_saida: 		PathBuf,
+                        expressao_n:	String,
+                        expressao_e:	String ) -> Result<()> {
 
 	let texto = fs::read_to_string(uri_entrada)?; // Tirei o método .unwrap() e coloquei o operador ?
 	let text = &String::from(texto);
@@ -27,38 +25,40 @@ pub fn analisa_texto (  uri_entrada: PathBuf,
 	// r"\d.\d{3}.\d{3},\d{3}" -> quando utilizado através do gtk_entry não foi
 	// necessário o caracteres r e as aspas, apenas
 	// a expressão regular propriamente dita.
-	// println!("Expressão latitude: {}", expressao_n);
-	// println!("Expressão longitude: {}", expressao_e);
-    for correspondencia in Regex::new(&expressao_n).unwrap().find_iter(text) {
-    	let start = correspondencia.start() as usize;
-    	let end = correspondencia.end() as usize;
-    	vetor1.push(&text[start..end]);
-    }
+
+  for correspondencia in Regex::new(&expressao_n).unwrap().find_iter(text) {
+  	let start = correspondencia.start() as usize;
+  	let end = correspondencia.end() as usize;
+  	vetor1.push(&text[start..end]);
+  }
 
 	for correspondencia in Regex::new(&expressao_e).unwrap().find_iter(text) {
     	let start = correspondencia.start() as usize;
     	let end = correspondencia.end() as usize;
 
     	vetor2.push(&text[start..end]);
-    }
+  }
 
 	let mut vetor3 = Vec::with_capacity(VEC_SIZE);
-    for x in vetor1.iter_mut() {
-    	vetor3.push(x.replace(".",""));
-    }
+  for x in vetor1.iter_mut() {
+  	vetor3.push(x.replace(".",""));
+  }
 
-    let mut vetor4 = Vec::with_capacity(VEC_SIZE);
-   	for x in vetor2.iter_mut() {
-    	vetor4.push(x.replace(".",""));
-    }
+  let mut vetor4 = Vec::with_capacity(VEC_SIZE);
+ 	for x in vetor2.iter_mut() {
+  	vetor4.push(x.replace(".",""));
+  }
 
-    gera_csv (vetor3, vetor4, uri_saida).expect("Não foi possível utilizar a uri informada pela função Dados::new()");
-    Ok(())
+  gera_csv (vetor3, vetor4, uri_saida)
+  	.expect("Não foi possível utilizar a uri informada pela função Dados::new()");
+
+  Ok(())
 }
 
 pub fn gera_csv (   vec: Vec<String>,
                     vec1: Vec<String>,
                     uri2: PathBuf ) -> Result<()> {
+
 	let mut wtr = Writer::from_path(uri2)?;
 	wtr.write_record(&["Latitude","Longitude"])?;
 
@@ -85,34 +85,30 @@ pub fn salva_perfis (serializado: String) -> std::io::Result<()> {
 }
 
 pub fn carrega_perfis () -> std::io::Result<String> {
-	let file = fs::read_to_string("perfis.yaml")?;
-	Ok(file)
+
+	Ok(fs::read_to_string("perfis.yaml")?)
 }
 
 #[allow(dead_code)]
-fn serializa (  map: &BTreeMap<String,
-                Expressoes>) -> String {
+fn serializa (  map: &BTreeMap<String, Expressoes>) -> String {
 
-	let serializado = serde_json::to_string(&map).unwrap();
-	serializado
+	serde_json::to_string(&map).unwrap()
 }
 
 pub fn serializa_yaml ( map: &BTreeMap<String, Expressoes>) -> String {
 
-	let serializado = serde_yaml::to_string(&map).unwrap();
-	serializado
+	serde_yaml::to_string(&map).unwrap()
 }
 
 #[allow(dead_code)]
 pub fn desserializa (serializado: String) -> BTreeMap<String, Expressoes> {
 
-	let desserializado: BTreeMap<String, Expressoes> = serde_json::from_str(&serializado).unwrap();
-	desserializado
+	serde_json::from_str(&serializado).unwrap()
 }
 
 pub fn desserializa_yaml (serializado: String) -> BTreeMap<String, Expressoes> {
-	let desserializado: BTreeMap<String, Expressoes> = serde_yaml::from_str(&serializado).unwrap();
-	desserializado
+
+	serde_yaml::from_str(&serializado).unwrap()
 }
 
 pub fn popula_perfis () -> BTreeMap<String, Expressoes> {
@@ -129,7 +125,7 @@ pub fn popula_perfis () -> BTreeMap<String, Expressoes> {
 }
 
 pub fn remove_perfil (	perfil_atual: String,
-						perfis: &Rc<RefCell<BTreeMap<String, Expressoes>>> ) {
+												perfis: &Rc<RefCell<BTreeMap<String, Expressoes>>> ) {
 
 	let mut map: RefMut<_> = perfis.borrow_mut();
 	map.remove(perfil_atual.as_str());
